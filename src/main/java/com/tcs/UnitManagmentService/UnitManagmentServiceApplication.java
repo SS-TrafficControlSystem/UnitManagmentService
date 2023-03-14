63691591d6b2fc554e309b57ea34a263d5a8b76b
ybart <y.bartoshchuk@gmail.com>
2023-03-14 14:08:21 +0200

    init commit

---
+++ src/main/java/com/tcs/UnitManagmentService/UnitManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.UnitManagmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/UnitManagmentService/UnitManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.UnitManagmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
